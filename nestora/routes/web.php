6c9a9d70a7fce9866b68ea88ecbf2889ca61f6be
jouel88 <muhammadjulianromadhoni@gmail.com>
2025-06-16 22:38:40 +0700

    perbaikan tulisan dashboardcontroller

--- nestora/routes/web.php
+++ nestora/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\dashboardcontroller;
 use App\Http\Controllers\DataMasterController;
 use App\Http\Controllers\DataUserController;
 use App\Http\Controllers\PredictionController;
